3f00945cf0895e638091be92cccfcc9c684fabc1
why <why.DK37@gmail.com>
2019-09-28 08:51:18 +0800

    init

---
+++ src/main/java/me/es/EsDemo.java
@@ -0,0 +1,37 @@
+package me.es;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+
+import java.io.IOException;
+
+public class EsDemo {
+    public static void main(String[] args) throws IOException {
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http")));
+
+        IndexRequest request = new IndexRequest("posts");
+        request.id("1");
+        String jsonString = "{" +
+                "\"user\":\"kimchy\"," +
+                "\"postDate\":\"2013-01-30\"," +
+                "\"message\":\"trying out Elasticsearch\"" +
+                "}";
+        request.source(jsonString, XContentType.JSON);
+
+        Map<String, Object> jsonMap = new HashMap<>();
+        jsonMap.put("user", "kimchy");
+        jsonMap.put("postDate", new Date());
+        jsonMap.put("message", "trying out Elasticsearch");
+        IndexRequest indexRequest = new IndexRequest("posts")
+                .id("1").source(jsonMap);
+
+        client.index(request, RequestOptions.DEFAULT);
+
+        client.close();
+    }
+}
