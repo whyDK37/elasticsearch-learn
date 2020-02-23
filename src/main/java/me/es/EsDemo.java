279675f3c584404e0fbf9e3175ce5e94cd81f791
why <why.DK37@gmail.com>
2020-02-23 20:49:28 +0800

    format

--- src/main/java/me/es/EsDemo.java
+++ src/main/java/me/es/EsDemo.java
@@ -1,5 +1,6 @@
 package me.es;
 
+import java.io.IOException;
 import org.apache.http.HttpHost;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.RequestOptions;
@@ -7,31 +8,30 @@ import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentType;
 
-import java.io.IOException;
-
 public class EsDemo {
-    public static void main(String[] args) throws IOException {
-        RestHighLevelClient client = new RestHighLevelClient(
-                RestClient.builder(new HttpHost("localhost", 9200, "http")));
 
-        IndexRequest request = new IndexRequest("posts");
-        request.id("1");
-        String jsonString = "{" +
-                "\"user\":\"kimchy\"," +
-                "\"postDate\":\"2013-01-30\"," +
-                "\"message\":\"trying out Elasticsearch\"" +
-                "}";
-        request.source(jsonString, XContentType.JSON);
+  public static void main(String[] args) throws IOException {
+    RestHighLevelClient client = new RestHighLevelClient(
+        RestClient.builder(new HttpHost("localhost", 9200, "http")));
 
-        Map<String, Object> jsonMap = new HashMap<>();
-        jsonMap.put("user", "kimchy");
-        jsonMap.put("postDate", new Date());
-        jsonMap.put("message", "trying out Elasticsearch");
-        IndexRequest indexRequest = new IndexRequest("posts")
-                .id("1").source(jsonMap);
+    IndexRequest request = new IndexRequest("posts");
+    request.id("1");
+    String jsonString = "{" +
+        "\"user\":\"kimchy\"," +
+        "\"postDate\":\"2013-01-30\"," +
+        "\"message\":\"trying out Elasticsearch\"" +
+        "}";
+    request.source(jsonString, XContentType.JSON);
 
-        client.index(request, RequestOptions.DEFAULT);
+    Map<String, Object> jsonMap = new HashMap<>();
+    jsonMap.put("user", "kimchy");
+    jsonMap.put("postDate", new Date());
+    jsonMap.put("message", "trying out Elasticsearch");
+    IndexRequest indexRequest = new IndexRequest("posts")
+        .id("1").source(jsonMap);
 
-        client.close();
-    }
+    client.index(request, RequestOptions.DEFAULT);
+
+    client.close();
+  }
 }
